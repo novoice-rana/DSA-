0a4c6065dec1d184ab3c783a6f1b584a86fa024e
novoice-rana <god0.x.trends@gmail.com>
2026-03-22 22:40:00 +0530

    dsa and vector stl

--- Array3/print2darr.cpp
+++ Array3/print2darr.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 #include<limits.h>
 // void printArray(int arr[][4],int row,int col ){
-//     for(int i=0;i<row;i++){
-//         for(int j=0;j<col;j++){
-//             cout<<arr[i][j]<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 // void colWisePrint(int arr[][4],int row,int col){
 //     for(int i=0;i<col;i++){
@@ -30,28 +31,62 @@ using namespace std;
 //     }
 //     return false;
 // }
-int findMax(int arr[][4],int row,int col){
-    int maxAns=INT_MIN;
-for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-        if(arr[i][j]>maxAns){
-            maxAns=arr[i][j];
-        }
-    }
-}
-return maxAns;
+// int findMax(int arr[][4],int row,int col){
+//     int maxAns=INT_MIN;
+// for(int i=0;i<row;i++){
+//     for(int j=0;j<col;j++){
+//         if(arr[i][j]>maxAns){
+//             maxAns=arr[i][j];
+//         }
+//     }
+// }
+// return maxAns;
 
-}
+// }
 
+
+
+// void transpose(int arr[][4],int row ,int col){
+// for (int i=0;i<row;i++){
+//     for (int j=i;j<col;j++){
+//         swap(arr[i][j],arr[j][i]);
+// }
+
+// }};
 int main(){
-int arr[3][4]={
-        {1,2,2,3},
-        {5,6,7,8},
-        {4,8,9,5}};
-        int row=3;
-        int col=4; 
-        cout<<findMax(arr,row,col)<<endl; 
+    //  int arr[4][4]={
+    //     {1,2,2,3},
+    //     {5,6,7,8},
+    //     {4,8,9,5},
+    //     {12,24,46,49}
+    // };
+    //     int row=4;
+    //     int col=4; 
+    //     transpose(arr,row,col);
+    //     printArray(arr,row,col);
+
+   
+    // 2D is_array 
+    // vector<vector<int>>arr(5,vector<int>(10,0));
+    //row size=arr.size()
+    // for(int  i = 0; i < arr.size(); i++){
+    //     for(int j=0;j<arr.size();j++){
+    //        cout<<arr[i][j]<<" "; 
+    //     }
+    //     cout<<endl;
+    // }
+    //jaged array
+    
+    
+    
+
+
+
 }
+      
+
+        // cout<<findMax(arr,row,col)<<endl; 
+
 
     
         // int target=99 ;
---
+++ Array3/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+l);
+        printArray(arr,row,c
