45a3279a4b00dc236a1fea170f6435987e1af215
Rana Pratap Sarangi <god0.x.trends@gmail.com>
2026-04-09 21:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (18.53%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,9 +3,10 @@ public:
     void rotate(vector<int>& nums, int k) {
        int n=nums.size();
         vector<int>ans(n);
-        for (int index=0;index<n;index++){
-            int newIndex=(index+k)%n;
-            ans[newIndex]=nums[index];
+        for( int i=0;i<n;i++){
+            int newIndex=(i+k)%n;
+            ans[newIndex]=nums[i];
+        
         }
         nums=ans;
          
