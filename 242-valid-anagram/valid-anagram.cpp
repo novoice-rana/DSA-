40a310f6f94149ce26afb9fc17ee71988c3a812b
Rana Pratap Sarangi <god0.x.trends@gmail.com>
2026-07-31 21:18:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (76.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
+       if (s.length() != t.length()) return false;
+    
+    // for lowercase a-z
+    vector<int> freq(26, 0);  
+    
+    // Count frequency of each character in s1
+    for(char ch : s) 
+        freq[ch - 'a']++;
+    
+    // Subtract frequency using characters from s2
+    for(char ch : t) 
+        freq[ch - 'a']--;
+    
+    // If any count is not zero, not an anagram
+    for(int count : freq) {
+        if (count != 0)
             return false;
-        }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s == t);
-        
+    }
+    
+    return true;
         
         
     }
