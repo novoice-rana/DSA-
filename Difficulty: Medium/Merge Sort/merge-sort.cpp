aecf88987d4b0114f6e621e6db5af525550c289c
Rana Pratap Sarangi <god0.x.trends@gmail.com>
2026-09-07 21:55:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    void merge(vector<int>& arr, int l, int mid, int r) {
+
+        int i = l;
+        int j = mid + 1;
+
+        vector<int> temp;
+
+        while (i <= mid && j <= r) {
+
+            if (arr[i] <= arr[j]) {
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else {
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+
+        while (i <= mid) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+
+        while (j <= r) {
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+        for (int k = 0; k < temp.size(); k++) {
+            arr[l + k] = temp[k];
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int l, int r) {
+
+        // Base case
+        if (l >= r)
+            return;
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+};
